e5665dfea3ade7ad4bcd452f27e8b616b4fe52e2
sha2nk <shashankyadav488@gmail.com>
2021-05-02 13:08:16 +0530

    Firebse Initialized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
-import './App.css';
+
 
 function App() {
   return (
     <div>
-      
+      adad
     </div>
   );
 }
---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyDRZCVnqnq0h2bf0_g2saxJtwBfXHREnZE",
+    authDomain: "chat-web-app-791c7.firebaseapp.com",
+    databaseURL: "https://chat-web-app-791c7-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-791c7",
+    storageBucket: "chat-web-app-791c7.appspot.com",
+    messagingSenderId: "832953670107",
+    appId: "1:832953670107:web:38f21dd94cfa7888f6affe"
+  };
+
+  const app = firebase.initializeApp(config); 
