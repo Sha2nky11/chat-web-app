c5187698787d9036a694660574b8fd4369cd76a2
sha2nk <shashankyadav488@gmail.com>
2021-05-03 02:13:54 +0530

    Added Authentication

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database'
 
 const config = {
     apiKey: "AIzaSyDRZCVnqnq0h2bf0_g2saxJtwBfXHREnZE",
@@ -11,3 +13,6 @@ const config = {
   };
 
   const app = firebase.initializeApp(config); 
+
+  export const auth = app.auth();
+  export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,60 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import { Container, Grid ,Panel,Row,Col, Button, Icon, Alert } from 'rsuite';
+import '../styles/utility.scss'
+import { auth, database } from '../misc/firebase';
 
 const SignIn = () => {
+
+    const signInWithProvider = async (provider) => {
+        try {
+            const {additionalUserInfo,user}= await auth.signInWithPopup(provider);
+        
+            if(additionalUserInfo.isNewUser){
+                database.ref(`/profiles/${user.uid}`).set({
+                    name: user.displayName,
+                    time : firebase.database.ServerValue.TIMESTAMP
+                })
+            }
+            Alert.success("Signed In",400);
+        } catch(err) {
+            Alert.error(err.message,400);
+        }
+       
+    }
+
+    const onFacebookSignIn = () => {
+        signInWithProvider(new firebase.auth.FacebookAuthProvider());
+    }
+
+    const onGoogleSignIn = () => {
+        signInWithProvider(new firebase.auth.GoogleAuthProvider());
+    }
+
+
     return (
-        <div>
-           SignIn Page 
-        </div>
+        <Container>
+            <Grid className="mt-page">
+                <Row>
+                    <Col xs={24} md={12} mdOffset={6}>
+                        <Panel>
+                            <div className="text-center">
+                                <h2>Chat Application</h2>
+                                <p>Progressive chat app</p>
+                            </div>
+                            <div className="mt-3">
+                                <Button block color="cyan" onClick = {onFacebookSignIn}>
+                                    <Icon icon="facebook"> Continue with facebook</Icon>
+                                </Button>
+                                <Button block color="violet" onClick = {onGoogleSignIn}>
+                                    <Icon icon="google"> Continue with google</Icon>
+                                </Button>
+                            </div>
+                        </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+        </Container>
     );
 }
 
