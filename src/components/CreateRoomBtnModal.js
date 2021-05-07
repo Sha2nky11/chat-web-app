bbdc91c0619be204a867f19a99395f6951169b67
sha2nk <shashankyadav488@gmail.com>
2021-05-07 22:14:27 +0530

    Added button to create new chat room

---
+++ src/components/CreateRoomBtnModal.js
@@ -0,0 +1,84 @@
+import React, { useCallback, useRef, useState } from 'react'
+import { Alert, Button, ControlLabel, Form, FormControl, FormGroup, Icon, Modal, Schema } from 'rsuite'
+import firebase from 'firebase/app'
+import {database} from '../misc/firebase' 
+import { useModalState } from '../misc/custom-hooks'
+
+
+
+const INITIAL_FORM = {name:'',description:''}
+const {StringType} = Schema.Types;
+
+const model = Schema.Model({
+    name : StringType().isRequired('Chat name is required'),
+    description : StringType().isRequired('Description is required')
+});
+
+const CreateRoomBtnModal = () => {
+
+    const {isOpen,open,close} = useModalState();
+    const [formValue,setFormValue] = useState(INITIAL_FORM);
+    const [isLoading , setIsLoading] = useState(false);
+    const formRef = useRef();
+
+    const onFormChange = useCallback((value) => {
+        setFormValue(value);
+    },[]);
+
+    const onSubmit = async () => {
+        if(!formRef.current.check()){
+            return;
+        }
+        setIsLoading(true);
+
+        const newRoomdata = {
+            ...formValue,
+            createdAt : firebase.database.ServerValue.TIMESTAMP,
+        }
+
+        try {
+            await database.ref('rooms').push(newRoomdata);
+            Alert.info(`${formValue.name} has been created `,5000);
+            setFormValue(INITIAL_FORM);
+            setIsLoading(false);
+            close();
+            
+        } catch (error) {
+            setIsLoading(false);
+            Alert.error(error.message,5000);
+        }
+
+    }
+
+    return (
+        <div>
+           <Button block onClick={open} color='orange' className='mt-1'>
+               <Icon icon='creative'/> Create new chat
+           </Button>
+
+           <Modal show={isOpen} onHide={close}>
+               <Modal.Header>
+                   <Modal.Title> New Chat Room </Modal.Title>
+                </Modal.Header>
+               <Modal.Body> 
+                    <Form fluid onChange={onFormChange} formValue={formValue} model={model} ref={formRef}>
+                        <FormGroup>
+                            <ControlLabel >Room Name</ControlLabel>
+                            <FormControl name="name" placeholder="Enter chat room name..." />
+                        </FormGroup>
+                        <FormGroup>
+                            <ControlLabel >Description</ControlLabel>
+                            <FormControl componentClass ="textarea" rows={5} name="description" placeholder="Enter description..." />
+                        </FormGroup>
+                    </Form>
+               </Modal.Body>
+               <Modal.Footer> 
+                   <Button block appearance='primary' color='green' onClick={onSubmit} disabled={isLoading}>Create new chat room now</Button>
+               </Modal.Footer>
+           </Modal>
+        </div>
+
+    )
+}
+
+export default CreateRoomBtnModal
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import DashboardToggle from './dashboard/DashboardToggle'
 import '../styles/utility.scss'
+import CreateRoomBtnModal from './CreateRoomBtnModal'
 
 const SideBar = () => {
     return (
         <div className ="h-100 pt-2">
             <div>
                 <DashboardToggle/>
+                <CreateRoomBtnModal/>
             </div>
             Bottom
         </div>
