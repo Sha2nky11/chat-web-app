e5dcd5dc11ff15633dd5278579e72ca75049e070
sha2nk <shashankyadav488@gmail.com>
2021-05-09 21:16:25 +0530

    created chat layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'rsuite/dist/styles/rsuite-default.css';
 import {ProfileProvider} from './context/profile.context'
 import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute'
-import Home from './pages/Home';
+import Home from './pages/Home/index';
 import PublicRoute from './components/PublicRoute';
 
 
