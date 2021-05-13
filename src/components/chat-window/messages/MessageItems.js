859c89e7b247d6573a48e78c58309df64074a7b0
sha2nk <shashankyadav488@gmail.com>
2021-05-14 01:22:48 +0530

    display realtime presence

--- src/components/chat-window/messages/MessageItems.js
+++ src/components/chat-window/messages/MessageItems.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TimeAgo from 'timeago-react';
+import Presence from '../../Presence';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
@@ -11,7 +12,8 @@ const MessageItems = ({message}) => {
     return (
         <div className="padded mb=1">
             <div className="d-flex align-items-center font-bolder mb-1"> 
-                <ProfileAvatar src = {author.avatar} name = {author.name} className="m1-1" size= "xs" />
+                <Presence uid = {author.uid}/>
+                <ProfileAvatar src = {author.avatar} name = {author.name} className="ml-1" size= "xs" />
                 <ProfileInfoBtnModal profile ={author} appearance ="link" className="p-0 ml-2 text-black" />
                 <TimeAgo  datetime={createdAt}  className="font-norml text-black-45 ml-2" />
             </div>
